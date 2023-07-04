2fab3b89ebb1d8fc6932313bcd8682c351267192
Sgnahore <stephaniergnahore@gmail.com>
2023-07-04 14:00:29 +0100

    VERSION C

--- src/components/TestButton.tsx
+++ src/components/TestButton.tsx
@@ -1,13 +1,10 @@
 function TestButton(): JSX.Element {
- const handleClick = () => {
-  console.log("Let me tell you something");
- }
- 
-  return (
-  <button onClick={handleClick}>
-    A cheeky test round 2
-    </button>
-  );
+  const handleClick = () => {
+    console.log("Let me tell you something");
+    window.open("Let me tell you something");
+  };
+
+  return <button onClick={handleClick}>A cheeky test round 2</button>;
 }
 
 export default TestButton;
