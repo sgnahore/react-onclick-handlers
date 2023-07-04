9ad7844ccf293bd11888a79beb24d91c47a7a1fc
Sgnahore <stephaniergnahore@gmail.com>
2023-07-04 12:34:40 +0100

    VERSION A

--- src/components/TestButton.tsx
+++ src/components/TestButton.tsx
@@ -1,5 +1,11 @@
 function TestButton(): JSX.Element {
-  return <button>Make me do something when I'm clicked...</button>;
+ function handleClick() {
+  console.log("Let me tell you something");
+ }
+ 
+  return (
+  <button onClick={handleClick}>A cheeky test</button>
+  );
 }
 
 export default TestButton;
